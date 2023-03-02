a3ac4196b8fa65b3778b8d038dfb9574da99517b
Yichen Huang <95195795+yichenhuang2000@users.noreply.github.com>
2023-03-02 13:48:45 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -4,7 +4,7 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 from spicy import integrate
 mpl.use('TkAgg')
-
+g = 9.8
 
 def read_acceleration():
     df = pd.read_csv('Acceleration without g 2023-03-01 18-32-06.csv')
@@ -22,7 +22,7 @@ def calculate_velocity_displacement(t, a):
     return v, x
 
 
-def draw_plot(t, a, v, x):
+def draw_a_v_x_over_t(t, a, v, x):
     t = np.array(t)
     a = np.array(a)
     plt.plot(t, a)
@@ -41,13 +41,48 @@ def draw_plot(t, a, v, x):
     plt.ylabel("displacement")
     plt.show()
 
+
+def get_KE_over_m(v):
+    return np.divide(np.power(v,2),2)
+
+
+def get_PE_over_m(H, x):
+    x = np.array(x)
+    H_array = np.empty(len(x))
+    H_array.fill(H)
+    h = H_array-x
+    return g*h
+
+
+def get_potential_sum(KE, PE):
+    return KE+PE
+
+def draw_KE_PE_over_t(KE, PE,E_sum, t):
+    KE = np.array(KE)
+    PE = np.array(PE)
+    E_sum = np.array(E_sum)
+    plt.plot(t, KE, label='KE / m')
+    plt.plot(t, PE, label='PE / m')
+    plt.plot(t, E_sum, label='(KE + PE) / m')
+    plt.title("energy vs time")
+    plt.xlabel("time")
+    plt.ylabel("energy per kilo")
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
     df = read_acceleration()
     t = df['t(s)'].tolist()
     a = df['a(m/s²)'].tolist()
-    print(t)
-    print(a)
+    #print(t)
+    #print(a)
     v, x = calculate_velocity_displacement(t, a)
-    draw_plot(t, a, v, x)
-    print(v)
-    print(x)
+    draw_a_v_x_over_t(t, a, v, x)
+    KE_over_m = get_KE_over_m(v)
+    PE_over_m = get_PE_over_m(1.32, x)
+    SUM = get_potential_sum(KE_over_m, PE_over_m)
+    #print(KE_over_m, PE_over_m, SUM)
+    draw_KE_PE_over_t(KE_over_m, PE_over_m, SUM, t)
+
+    #print(v)
+    #print(x)
